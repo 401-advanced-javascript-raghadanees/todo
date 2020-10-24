061d41d0217127e6fc0b93f079b015276ee0a2c4
raghadanees <raghad.anees@hotmail.com>
2020-10-24 13:43:43 +0300

    Updates

---
+++ precache-manifest.005bd0a6dc61120ffc68a755c3674db8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32b45b3c7c4dbe882f5447707f8de624",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "de88211f484140b1b520",
+    "url": "/todo/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "ab77c17c60952ebbdf0b",
+    "url": "/todo/static/css/main.e3eb8ce1.chunk.css"
+  },
+  {
+    "revision": "de88211f484140b1b520",
+    "url": "/todo/static/js/2.5d0dd39f.chunk.js"
+  },
+  {
+    "revision": "1d86523a7e9050966b444295ca01b1e7",
+    "url": "/todo/static/js/2.5d0dd39f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab77c17c60952ebbdf0b",
+    "url": "/todo/static/js/main.47808417.chunk.js"
+  },
+  {
+    "revision": "4b7c96a078d3292937b1",
+    "url": "/todo/static/js/runtime-main.d4ce9cd5.js"
+  }
+]);
